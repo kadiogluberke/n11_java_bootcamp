77572a1062164266e56b6065b4d12ef8460e490a
Berke Kadioglu <kadiogluberke@gmail.com>
2026-04-19 19:57:00 +0300

    Java project structure

---
+++ n11-b/src/main/java/com/bkadioglu/App.java
@@ -0,0 +1,13 @@
+package com.bkadioglu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
