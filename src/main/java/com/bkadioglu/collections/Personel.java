cf739a992586c5cf1d21762501ace041bc38e76e
Berke Kadioglu <kadiogluberke@gmail.com>
2026-04-21 10:25:25 +0300

    2.GUN: java collections

---
+++ src/main/java/com/bkadioglu/collections/Personel.java
@@ -0,0 +1,14 @@
+package com.bkadioglu.collections;
+
+public class Personel {
+
+    String ad;
+    String soyad;
+    double maas;
+
+    public Personel(String ad, String soyad, double maas) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.maas = maas;
+    }
+}
