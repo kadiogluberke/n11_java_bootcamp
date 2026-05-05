ec8361dc557f12c67cfefa4d988c23d86993db91
Berke Kadioglu <kadiogluberke@gmail.com>
2026-05-05 23:33:24 +0300

    microservices

---
+++ microservices/config-server/src/test/java/com/bkadioglu/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkadioglu.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
